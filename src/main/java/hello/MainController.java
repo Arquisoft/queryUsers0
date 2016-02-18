122df2c4af45485ef52866a63880ee56a4c3eb30
Labra <jelabra@gmail.com>
2016-02-18 10:44:27 +0100

    Added templates...

--- src/main/java/hello/MainController.java
+++ src/main/java/hello/MainController.java
@@ -1,19 +1,21 @@
 package hello;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
-
-    @RequestMapping("/user")
-    public UserInfo user() {
-        return new UserInfo("pepe",0);
-    }
 
     @RequestMapping("/")
     public String landing() {
-        return "User Management Service";
+        return "saludo";
     }
+    
+    @RequestMapping("/hola")
+    public String hola() {
+        return "saludo";
+    }
+
 }
